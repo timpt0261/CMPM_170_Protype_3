3782fed3ae36e3d1cce768d20a22492211d9bd56
timpt0261 <rchavez0215@gmail.com>
2023-12-06 12:33:23 -0800

    Changing UI color over health

--- CMPM_170_Prototype_3/Assets/Scripts/HealthBar.cs
+++ CMPM_170_Prototype_3/Assets/Scripts/HealthBar.cs
@@ -8,23 +8,35 @@ using System;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] float currentHealth = 100f;
+    [Range(1, 10)]
+    [SerializeField] private int multiplier = 1;
     [SerializeField] Image fillImage;
     
     void Start()
     {
+        fillImage.fillAmount = currentHealth;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentHealth -= Time.deltaTime;
+        if (currentHealth <= 0) Debug.Log("GameOver");
+        currentHealth -= Time.deltaTime * multiplier;
         UpdateHealth();
         
     }
 
     private void UpdateHealth()
     {
-        fillImage.fillAmount = currentHealth / 100;
+        UpdateHealthColor();
+        fillImage.fillAmount = currentHealth;
+    }
+
+    private void UpdateHealthColor()
+    {
+        if (currentHealth < 20)
+            fillImage.color = Color.red;
+ 
     }
 }
