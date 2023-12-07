c56b76fd5b4c5149c5f4392e04fc3fbde8afee9a
timpt0261 <rchavez0215@gmail.com>
2023-12-07 01:39:20 -0800

    Added Heath Bar and Death Logic

--- CMPM_170_Prototype_3/Assets/Scripts/HealthBar.cs
+++ CMPM_170_Prototype_3/Assets/Scripts/HealthBar.cs
@@ -1,42 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TMPro;
-using System;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] float currentHealth = 100f;
     [Range(1, 10)]
     [SerializeField] private int multiplier = 1;
+    [SerializeField] Image panelImage;
     [SerializeField] Image fillImage;
-    
+
+    private Color initialImageColor = new Color(0.9529412f, 1f, 0.003921569f);
+
     void Start()
     {
-        fillImage.fillAmount = currentHealth;
-        
+        // Set the initial alpha of the panelImage to 0
+        Color panelColor = panelImage.color;
+        panelColor.a = 0f;
+        panelImage.color = panelColor;
+
+        fillImage.color = initialImageColor;
+        fillImage.fillAmount = 1;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (currentHealth <= 0) Debug.Log("GameOver");
+        if (currentHealth <= 0)
+        {
+            Debug.Log("GameOver");
+            SceneManager.LoadScene(1);
+        }
+
         currentHealth -= Time.deltaTime * multiplier;
         UpdateHealth();
-        
     }
 
     private void UpdateHealth()
     {
-        UpdateHealthColor();
-        fillImage.fillAmount = currentHealth;
-    }
+        float normalizedHealth = currentHealth / 100f;
 
-    private void UpdateHealthColor()
-    {
-        if (currentHealth < 20)
-            fillImage.color = Color.red;
- 
+        // Invert the normalized health to get a more noticeable color transition
+        float invertedNormalizedHealth = 1f - normalizedHealth;
+
+        // Update the alpha value of panelImage.color
+        Color panelColor = panelImage.color;
+        panelColor.a = Mathf.Lerp(0f, 1f, invertedNormalizedHealth);
+        panelImage.color = panelColor;
+
+        // Create a new color with the same RGB values as initialImageColor but with alpha set to 1
+        Color targetColor = new Color(initialImageColor.r, initialImageColor.g, initialImageColor.b, 1f);
+
+        fillImage.color = Color.Lerp(targetColor, Color.red, invertedNormalizedHealth);
+        fillImage.fillAmount = normalizedHealth;
     }
 }
