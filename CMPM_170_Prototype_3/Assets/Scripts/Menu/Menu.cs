9f7fdbe2a91493579c22bbcf7a1cd20ef2422bfd
timpt0261 <rchavez0215@gmail.com>
2023-12-09 14:26:25 -0800

    Added Menu Script to manage scenes

---
+++ CMPM_170_Prototype_3/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    public void GoToPrototype1() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToPrototype2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToPrototype3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            ExitGame();
+        }
+    }
+}
