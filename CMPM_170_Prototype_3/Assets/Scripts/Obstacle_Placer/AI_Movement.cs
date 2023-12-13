b6b19cdfa3e2939ec2547da1f045671e07cb9a6c
timpt0261 <rchavez0215@gmail.com>
2023-12-12 18:28:34 -0800

    Updated Scene for Protoscene3

--- CMPM_170_Prototype_3/Assets/Scripts/Obstacle_Placer/AI_Movement.cs
+++ CMPM_170_Prototype_3/Assets/Scripts/Obstacle_Placer/AI_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AImovement : MonoBehaviour
+public class AI_Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
